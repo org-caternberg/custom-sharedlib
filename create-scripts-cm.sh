94a130189a5c0ecc3f0679c6219f2240e669e5e1
andreas <caternberg@gmx.de>
2023-04-24 14:55:26 +0200

    switch ns

--- create-scripts-cm.sh
+++ create-scripts-cm.sh
@@ -3,6 +3,7 @@
 #see https://kubernetes.io/docs/tasks/configure-pod-container/configure-pod-configmap/#populate-a-volume-with-data-stored-in-a-configmap
 CM=pod-build-scripts
 VERSION=0.1
+kubens cloudbees-core
 kubectl delete cm $CM
 kubectl create cm $CM  --from-file=resources/scripts/
 kubectl label --overwrite cm $CM version=$VERSION
