736219656591d8f20a4716abbef07d2fff90f8cb
andreas <caternberg@gmx.de>
2023-04-25 18:08:11 +0200

    coment

--- vars/newSemVersion.groovy
+++ vars/newSemVersion.groovy
@@ -2,7 +2,7 @@ def call(String version) {
     echo "param world: ${version}"
     def result = sh(returnStdout: true, script: "newSematicVersion.sh -m ${version}")
     reesult=result.substring(result.lastIndexOf("##RESULT##:") + 1)
-    echo result
+    //echo result
     env.NEW_VERSION=result
     return result
 }
