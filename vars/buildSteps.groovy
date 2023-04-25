d2b760adc63a9acf1c547a67f32fd20b76c8ffc0
andreas <caternberg@gmx.de>
2023-04-25 15:32:52 +0200

    fix

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -1,7 +1,8 @@
 def call() {
     def new_version=newSemVersion "1.0.1"
-    echo "1.0.1 -> $NEW_VERSION"
-    mvnVersionsSet "${new_version}"
-    //OR use env variable: mvnVersionsSet $NEW_VERSION
+    echo "1.0.1 -> $NEW_VERSION = ${new_version}"
+    //mvnVersionsSet "${new_version}"
+    //OR use env variable:
+    mvnVersionsSet $NEW_VERSION
     mvn "clean install"
 }
