6482cdb4c826752cc52c5016b755d446412fa347
andreas <caternberg@gmx.de>
2023-04-25 17:52:53 +0200

    quit mode

--- vars/mvnVersionsSet.groovy
+++ vars/mvnVersionsSet.groovy
@@ -1,3 +1,3 @@
 def call(String version){
-  	sh "mvn  versions:set -DprocessAllModules=true -DnewVersion=${version}"
+  	sh "mvn -q versions:set -DprocessAllModules=true -DnewVersion=${version}"
 }
