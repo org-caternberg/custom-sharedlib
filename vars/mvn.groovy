88bf8ffa6092a208c3bb81039a68453fee8b0243
andreas <caternberg@gmx.de>
2023-04-25 10:10:20 +0200

    add function

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,8 +1,16 @@
-def call(String goals) {
+def call(String goals = "clean install") {
     echo "param goals: ${goals}"
     // sh "helloworld.sh ${world}"
     configFileProvider([configFile(fileId: 'global-maven-settings', variable: 'MAVEN_SETTINGS_XML')]) {
         sh "cat ${MAVEN_SETTINGS_XML}"
+        sh "mvn -q -s ${MAVEN_SETTINGS_XML} ${goals}"
+    }
+}
+
+def withConfigFileProvider(String goals  = "clean install",String configFileProviderID = "global-maven-settings") {
+    echo "param goals: ${goals}, configFileProviderID: ${configFileProviderID}"
+    configFileProvider([configFile(fileId: "${configFileProviderID}", variable: 'MAVEN_SETTINGS_XML')]) {
+        //sh "cat ${MAVEN_SETTINGS_XML}"
         sh "mvn -q -s ${MAVEN_SETTINGS_XML} ${goals}"
     }
 }
