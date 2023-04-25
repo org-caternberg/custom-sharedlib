ba4a207011c728cfe7314e5d4a0b5b8bb2ffe3bc
andreas <caternberg@gmx.de>
2023-04-25 17:56:24 +0200

    quit mode

--- vars/stepsBuildMaven.groovy
+++ vars/stepsBuildMaven.groovy
@@ -1,5 +1,7 @@
 def call() {
-    def new_version=newSemVersion "1.0.1"
+    pom = readMavenPom(file: 'pom.xml')
+    def pom_version = pom.version
+    def new_version=newSemVersion pom_version
     echo "1.0.1 -> $NEW_VERSION = ${new_version}"
     mvnVersionsSet "${new_version}"
     mvn "clean install"
