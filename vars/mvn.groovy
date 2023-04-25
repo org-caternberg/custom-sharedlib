60c0acf83233f828c60a0db2df119f0ff2644b24
andreas <caternberg@gmx.de>
2023-04-25 10:15:24 +0200

    update params

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,10 +1,6 @@
 def call( goals = "clean install") {
     echo "param goals: ${goals}"
-    // sh "helloworld.sh ${world}"
-    configFileProvider([configFile(fileId: 'global-maven-settings', variable: 'MAVEN_SETTINGS_XML')]) {
-        sh "cat ${MAVEN_SETTINGS_XML}"
-        sh "mvn -q -s ${MAVEN_SETTINGS_XML} ${goals}"
-    }
+    sh "mvn -q -${goals}"
 }
 
 def withConfigFileProvider( goals  = "clean install", configFileProviderID = "global-maven-settings") {
