5235e93ad1e8c3d858393ef13e99e63008518e95
andreas <caternberg@gmx.de>
2023-04-26 13:31:50 +0200

    update

--- vars/bbResponseHandlerJQ.groovy
+++ vars/bbResponseHandlerJQ.groovy
@@ -1,7 +1,7 @@
 def call() {
     //Fake some Git JSON response data
     def response = sh(returnStdout: true, script: "curl -s https://www.githubstatus.com/api/v2/status.json")
-    sh "echo ${response} -n | jq"
+    sh "echo  -n ${response} | jq"
     def result = sh "echo -n  ${response} | jq '.page.name'"
     //Or: for more complex lines in shell, externalize to shell script f.e.
     // def result = sh(returnStdout: true, script: "jq-sample-step.sh ${jsonData) }${jqargs}")
