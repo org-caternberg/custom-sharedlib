37f40df93b571d61bb625c95f78d0d7d4e609b89
andreas <caternberg@gmx.de>
2023-05-26 15:21:14 +0200

    update

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -17,7 +17,8 @@ pipeline {
                 steps {
                     script {
                         //read pipeline.yaml properties
-                        ci = readYaml file: "ci.yaml"fix
+                        ci = readYaml file: "ci.yaml"
+                        echo "${ci}"
                         //sample common setting
                         properties([parameters([string(defaultValue: 'value1', description: 'desc1', name: 'param1', trim: true)])])
                         for(int param in ci.params) {
