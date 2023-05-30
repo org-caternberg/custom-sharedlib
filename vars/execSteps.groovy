18f66232449f06a6302e0af9b931a861801b08ca
andreas <caternberg@gmx.de>
2023-05-30 15:08:59 +0200

    rename

--- vars/execSteps.groovy
+++ vars/execSteps.groovy
@@ -3,11 +3,12 @@
 def call() {
        ci = readYaml file: "ci.yaml"
         echo "${ci.steps}"
+    steps = Arrays.asList(ci.steps)
       // steps = ci.steps
       //& echo steps
-    /*   steps.each {
+       steps.each {
               echo it.name
               sh it.exec
-       }*/
+       }
  }
 
