a7c107fd1e7ab86acbb22dbf04bed50f518cc44a
andreas <caternberg@gmx.de>
2023-05-30 14:55:06 +0200

    rename

--- vars/execSteps.groovy
+++ vars/execSteps.groovy
@@ -1,7 +1,6 @@
 //https://blog.jdriven.com/2020/03/groovy-goodness-parse-yaml-with-yamlslurper/
 //import groovy.yaml.YamlSlurper
 def call() {
-       echo  $stage
        ci = readYaml file: "ci.yaml"
        steps = ci.steps
        steps.each {
