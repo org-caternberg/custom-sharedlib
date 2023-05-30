d9738f4f5664c16794f72619fae9eee61b160b17
andreas <caternberg@gmx.de>
2023-05-30 19:52:41 +0200

    add stages

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -3,33 +3,63 @@ def mavenPod = libraryResource 'podagents/podTemplate-prod.yaml'
 
 //https://github.com/devopscube/declarative-pipeline-examples/blob/master/parameters/Jenkinsfile.ActiveChoiceParameters
 //TODO:
-properties(
+/*properties(
         [
                 parameters([
                         string(defaultValue: '/data', name: 'Directory'),
                         string(defaultValue: 'Dev', name: 'DEPLOY_ENV')
                 ])
         ]
+)*/
+
+//see https://stackoverflow.com/questions/44570163/jenkins-dynamic-declarative-pipeline-parameters
+properties([
+        [
+                $class: 'RebuildSettings', autoRebuild: true, rebuildDisabled: false],
+        parameters(
+                script {
+                    listParameters()
+                }
+        )
+]
 )
-def ci=null;
+
+def ci = null;
+
+def list_arguments(){
+    lista = return_list()
+    if ("${JOB_NAME}".contains("word")){
+        ch = [
+                choice(choices: ['opt1', 'opt2', 'opt3'], description: 'desc', name: 'bla'),
+                choice(choices: ["${lista}"], description: 'some letter', name: 'ble')
+        ]
+
+    }else{
+        ch = [
+                choice(choices: ['opt1', 'opt2', 'opt3'], description: 'desc', name: 'bla')
+        ]
+    }
+    return ch
+
+}
 pipeline {
     agent none
-        stages {
-            stage('Init'){
-                agent {
-                    kubernetes {
-                        //#deprectatedlabel 'mavenPod'
-                        defaultContainer "build"
-                        yaml mavenPod
-                    }
+    stages {
+        stage('Init') {
+            agent {
+                kubernetes {
+                    //#deprectatedlabel 'mavenPod'
+                    defaultContainer "build"
+                    yaml mavenPod
                 }
-                steps {
+            }
+            steps {
 
-                    script {
-                        //read pipeline.yaml properties
-                        ci = readYaml file: "ci.yaml"
-                        echo ci.params
-                        //sample common setting
+                script {
+                    //read pipeline.yaml properties
+                    ci = readYaml file: "ci.yaml"
+                    echo ci.params
+                    //sample common setting
 /*                        properties(
                                 [parameters(
                                         [string(defaultValue: 'value1', description: 'desc1', name: 'param1', trim: true)
@@ -37,20 +67,20 @@ pipeline {
                                         ]
                                 )]
                         )*/
-                    }
-                }
-            }
-            stage('build'){
-                agent {
-                    kubernetes {
-                        //#deprectatedlabel 'mavenPod'
-                        defaultContainer "build"
-                        yaml mavenPod
-                    }
-                }
-                steps {
-                    execSteps "build"
                 }
             }
         }
+        stage('build') {
+            agent {
+                kubernetes {
+                    //#deprectatedlabel 'mavenPod'
+                    defaultContainer "build"
+                    yaml mavenPod
+                }
+            }
+            steps {
+                execSteps "build"
+            }
+        }
+    }
 }
