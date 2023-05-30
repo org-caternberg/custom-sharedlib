6ef92e66cff903cc61dc0c477a0944f536e204eb
andreas <caternberg@gmx.de>
2023-05-30 16:18:28 +0200

    add stages

--- vars/execSteps.groovy
+++ vars/execSteps.groovy
@@ -4,14 +4,19 @@ def call(stageName) {
     ci = readYaml file: "ci.yaml"
     //echo "${ci.stage[0].steps}"
     ci.stage.each { stage ->
-        if (stage.name == "$stageName") {
+        echo "first $stage"
+        if (stage.name == stageName) {
+            echo "found $stage"
             stage.steps.each { step ->
                 echo step.name
                 echo step.exec
                 //sh "${step.exec}"
                 evaluate(step.exec)
+
             }
+            return 0;
         }
+        echo "Not found $stageName"
     }
 }
 
