b80e5b1cd61fbde9c2bbc695167da434e4a7afb5
andreas <caternberg@gmx.de>
2023-05-30 16:14:53 +0200

    add stages

--- vars/execSteps.groovy
+++ vars/execSteps.groovy
@@ -1,13 +1,17 @@
 //https://blog.jdriven.com/2020/03/groovy-goodness-parse-yaml-with-yamlslurper/
 //import groovy.yaml.YamlSlurper
-def call() {
-      ci = readYaml file: "ci.yaml"
-      //echo "${ci.steps}"
-      ci.steps.each { step ->
-           echo step.name
-           echo step.exec
-           //sh "${step.exec}"
-           evaluate(step.exec)
-       }
- }
+def call(stageName) {
+    ci = readYaml file: "ci.yaml"
+    //echo "${ci.stage[0].steps}"
+    ci.stage.each { stage ->
+        if (stage.name == "$stageName") {
+            stage.steps.each { step ->
+                echo step.name
+                echo step.exec
+                //sh "${step.exec}"
+                evaluate(step.exec)
+            }
+        }
+    }
+}
 
