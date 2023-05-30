005b359bf3076e9a7557b6eec018b372664eb6ce
andreas <caternberg@gmx.de>
2023-05-30 15:20:14 +0200

    rename

--- vars/execSteps.groovy
+++ vars/execSteps.groovy
@@ -2,11 +2,15 @@
 //import groovy.yaml.YamlSlurper
 def call() {
       ci = readYaml file: "ci.yaml"
-      echo "${ci.steps}"
+      //echo "${ci.steps}"
       ci.steps.each { step ->
            echo step.name
            echo step.exec
-           // sh step.exec
+           sh "${step.exec}"
        }
+/*    def list = [1,2,3,4]
+    for(item in list){
+        println item
+    }*/
  }
 
