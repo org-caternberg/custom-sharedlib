c3f4c83afa87f22bbac75c6609444c1934988ca0
andreas <caternberg@gmx.de>
2023-11-16 18:22:41 +0100

    fix

--- PipelineReadConfig.groovy
+++ PipelineReadConfig.groovy
@@ -31,7 +31,7 @@ def generateDynamicEnvVars() {
     envVars['DYNAMIC_VARIABLE'] = 'dynamic_value'
     envVars['ANOTHER_VARIABLE'] = 'another_dynamic_value'
     // ADD CUSTOM  dynamic environment variables
-    loadValuesYaml().enviroment.eacheach { key, value ->
+    loadValuesYaml().enviroment.each { key, value ->
         envVars["$key"] = "$value"
     }
     // Add more dynamic variables as needed
